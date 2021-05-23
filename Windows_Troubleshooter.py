831e19b1da382c7f4c9043a503d0adbe126fdd14
Erdem <245907@student.pwr.edu.pl>
2021-05-24 00:05:03 +0200

    Finished 1.0v

---
+++ Windows_Troubleshooter.py
@@ -0,0 +1,13 @@
+import os
+import time
+def windows_diagnostics():
+    for i in range(0,10):
+        print('Looking for problems')
+        print('...')
+        time.sleep(2)
+    print("Seems like I'm finding some solution hango on!")
+    time.sleep(10)
+    print('Unable to fix this issue')
+    time.sleep(2)
+    print("Sorry for the time I stole from your life")
+windows_diagnostics()
